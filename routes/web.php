3c6bc2901992ffc747cbeaa05956fd6a2b72a2a5
Lunadiotic <ahmadhakim404@gmail.com>
2021-04-27 21:48:34 +0700

    menampilkan data response json

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    // return [
+    //     'message' => 'Hello, World'
+    // ];
+
+    return response()->json([
+        'message' => 'Hello, world'
+    ], 201);
+});
