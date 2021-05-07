b8a444b3626a117e8c00696e3f85beaffb5798f6
lionatic <ahmadhakim404@gmail.com>
2021-05-07 13:29:34 +0700

    mengatasi validasi unik

--- app/Http/Requests/TaskRequest.php
+++ app/Http/Requests/TaskRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class TaskRequest extends FormRequest
 {
@@ -23,9 +24,13 @@ class TaskRequest extends FormRequest
      */
     public function rules()
     {
+        $rule_task_unique = Rule::unique('tasks', 'task');
+        if ($this->method() !== 'POST') {
+            $rule_task_unique->ignore($this->route()->parameter('id'));
+        }
         return [
             'user' => ['required'],
-            'task' => ['required', 'min:3']
+            'task' => ['required', 'min:3', $rule_task_unique]
         ];
     }
 
