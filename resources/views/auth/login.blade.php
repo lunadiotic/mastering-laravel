fbcfc0cb3854ff52266d18ff5c5d82e415bbe937
lionatic <ahmadhakim404@gmail.com>
2021-05-10 11:08:46 +0700

    membuat fitur login

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('main')
+
+<div class="mt-5 mx-auto" style="width: 380px">
+    <div class="card">
+        <div class="card-body">
+            <form action="{{ url('/login') }}" method="POST">
+                @csrf
+                <div class="mb-3">
+                    <label for="" class="form-label">E-Mail</label>
+                    <input name="email" type="text" class="form-control" value="{{ old('email') }}">
+                    @error('email')
+                    <span class="text-danger">
+                        {{ $message }}
+                    </span>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="" class="form-label">Password</label>
+                    <input name="password" type="password" class="form-control" value="{{ old('password') }}">
+                    @error('password')
+                    <span class="text-danger">
+                        {{ $message }}
+                    </span>
+                    @enderror
+                </div>
+                <button type="submit" class="btn btn-primary">Login</button>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
